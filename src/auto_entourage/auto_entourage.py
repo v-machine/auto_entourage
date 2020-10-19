36c4bb28b5686a9149094907e5348834c46d5b3e
Vincent Mai <44751268+v-machine@users.noreply.github.com>
2020-10-19 01:39:02 -0400

    refactored context managers to ghutil

--- src/auto_entourage/auto_entourage.py
+++ src/auto_entourage/auto_entourage.py
@@ -5,9 +5,9 @@
         region: A region delineated by a closed curve.
         num: The number of entourages within the region.
         point: (Optional) The points where the entourage is anchored.
-        layer_name: Default to "Entourages" if not speficied.
-        seed: (Optional) Sets random seed entourage population
-        place: Loads entourages in a new layer. Deletes entourages and layer if false.
+        layer_name: Default to "Entourage" if not speficied.
+        seed: (Optional) Sets random seed. 
+        place: Loads entourages in a new layer.
 """
 __author__ = "Vincent Mai"
 __version__ = "0.1.0"
@@ -17,56 +17,20 @@ import Rhino
 import rhinoscriptsyntax as rs
 import scriptcontext as sc
 import Rhino.Geometry as rg
-import Rhino.RhinoDoc
 import ghpythonlib.components as ghc
-import ghpythonlib.treehelpers as th
 import Grasshopper.Kernel as ghk
 import random
-import math
 import os
-import inspect
-from treehandler import TreeHandler
-from Grasshopper import DataTree
+from ghutil import RhinoDocContext, NewLayerContext, TreeHandler
 
 RANDOM_SEED = 0
 UNIT_Z = (0, 0, 1)
-GH_DOC = sc.doc
 
-# TODO: create new_layer context
-# main():
-#   with inNewLayer(layer_name):
-#       func()
-layer_name = layer_name.AllData()       
-if not layer_name: 
-    layer_name = "Entourage"
-else:
-    layer_name = layer_name[0]
-    
-if not seed:
-    seed = RANDOM_SEED
-
-# def rhinoDocContext(func):
-#     """Decorator to switch ghdoc context to rhinodoc
-#     
-#     # TODO: create context manager
-#     """
-#     def wrapper(*args, **kwargs):
-#         sc.doc = Rhino.RhinoDoc.ActiveDoc
-#         func(*args, **kwargs)
-#         sc.doc = GH_DOC
-#     return wrapper
-
-class RhinoDocContext:
-    def __enter__(self):
-        sc.doc = Rhino.RhinoDoc.ActiveDoc
-    def __exit__(self, type, value, traceback):
-        sc.doc = GH_DOC
-        
-# @rhinoDocContext
 def addPictureFrame(path, point, normal, width, height):
     """Calls rhinoscriptsyntax's addPictureFrame method 
     
-    Creates a picture frame in Rhino document and centers it around a given point. The picture frame will always be vertical and are not cached in grasshopper.
+    Creates a picture frame in Rhino and centers it around a given point. The 
+    picture frame will always be vertical and are not cached in grasshopper.
     
     Args:
         path (str): path to .png image
@@ -74,9 +38,6 @@ def addPictureFrame(path, point, normal, width, height):
         normal (rg.Vector3d): xy-orientation of the picture frame
         width (float): width of the picture frame
         height (float): height of the picture fram
-    
-    Note:
-        Function call must be made in Rhino.RhinoDoc.ActiveDoc
     """
     with RhinoDocContext():
         x_axis = rs.VectorRotate(normal, 90, UNIT_Z)
@@ -84,7 +45,7 @@ def addPictureFrame(path, point, normal, width, height):
         point -= 0.5*x_axis*width
         plane = rg.Plane(point, x_axis, y_axis)
         sc.doc.ActiveDoc.Objects.AddPictureFrame(plane, path, False, width, 
-                                             height, False, False)
+                                                 height, False, False)
             
 def getFiles(path):
     """Returns a list of paths to PNGs from a directory
@@ -124,7 +85,7 @@ def scaleImage(baseWidth, baseHeight, targetHeight):
 def getCameraDirection():
     """Returns the camera direction of the active viewport
     """
-    cameraDir = GH_DOC.Views.ActiveView.ActiveViewport.CameraDirection
+    cameraDir = sc.doc.Views.ActiveView.ActiveViewport.CameraDirection
     projCameraDir = rg.Vector3d(cameraDir.X, cameraDir.Y, 0)
     projCameraDir.Unitize()
     return projCameraDir
@@ -146,7 +107,9 @@ def populateRegion(region, num):
 def placeImage(path, pt, normal, img_height):
     """Orients, scales, and places the input image as a PictureFrame
     
-    Orients the input image based on normal, scales it to the target height and centers it on the input point.
+    Orients the input image based on normal, scales it to the target height 
+    and centers it on the input point.
+   
     Args:
         img(str): path to the .png image 
         pt (rg.Point3d): the anchor to center the PictureFrame
@@ -161,31 +124,7 @@ def placeImage(path, pt, normal, img_height):
     except:
         print("Failed to process {}".format(path))
                     
-# def inNewLayer(layer_name):
-#     """Decorator to call a function in a new layer
-#     
-#     Args:
-#         layer_name (str): the name of the new layer
-#     """
-#     def wrap_func(func):
-#         def wrapper(*args, **kwargs):
-#             createAndSetCurrentLayer(layer_name)
-#             func(*args, **kwargs)
-#             resetLayer()
-#         return wrapper
-#     return wrap_func
-class NewLayer:
-    """Context Manager to call a function in a new layer
-    """
-    def __init__(self, layer_name):
-        self.layer_name = layer_name
-
-    def __enter__(self):
-        createAndSetCurrentLayer(self.layer_name)
-
-    def __exit__(self, type, value, traceback):
-        resetLayer()
-
+        
 @TreeHandler
 def loadImage(path, num):
     """Randomly choose a number of images from the given file path
@@ -201,7 +140,6 @@ def loadImage(path, num):
     imgList = random.sample(imgList, len(imgList))
     return [imgList[i % len(imgList)] for i in range(num)]
 
-# @inNewLayer(layer_name)
 def populate(path, img_height, region, point, num):
     """Populate a region (closed curve) with vertical PictureFrames
     
@@ -212,7 +150,7 @@ def populate(path, img_height, region, point, num):
         point (rg.Point3d): the anchor point of the picture frame
         num (int): the num of PictureFrames to populate a region
     """
-    with NewLayer(layer_name):
+    with NewLayerContext(layer_name):
         cameraDirection = getCameraDirection()
         if region.AllData():
             pts = populateRegion(region, num)
@@ -223,52 +161,6 @@ def populate(path, img_height, region, point, num):
             imgs = loadImage(path, num)
             placeImage(imgs, point, cameraDirection, img_height)
        
-# @rhinoDocContext
-def createAndSetCurrentLayer(layer_name):
-    """Creates a new layer and set it as the current layer
-    
-    Args:
-        layer_name (str): the name of the layer
-    
-    Note:
-        Function call must be made in Rhino.RhinoDoc.ActiveDoc
-    """
-    with RhinoDocContext():
-        layer = sc.doc.Layers.FindName(layer_name)
-        if layer is None:
-            layer_idx = sc.doc.Layers.Add(layer_name, System.Drawing.Color.Black)
-        else:
-            layer_idx = layer.Index
-        sc.doc.Layers.SetCurrentLayerIndex(layer_idx, True)
-        
-# @rhinoDocContext
-def resetLayer():
-    """Resets current layer to the default (first) layer
-    
-    Note:
-        Function call must be made in Rhino.RhinoDoc.ActiveDoc
-    """
-    with RhinoDocContext():
-        sc.doc.Layers.SetCurrentLayerIndex(0, True)
-        
-# @rhinoDocContext
-def deleteLayer(layer_name):
-    """Deletes a layer and all objects inside
-    
-    Args:
-        layer_name (str): the name of the layer
-    
-    Note:
-        Function call must be made in Rhino.RhinoDoc.ActiveDoc
-    """
-    with RhinoDocContext():
-        rhinoObjects = sc.doc.Objects.FindByLayer(layer_name)
-        if rhinoObjects:
-            for obj in rhinoObjects:
-                sc.doc.Objects.Delete(obj)
-        sc.doc.Layers.SetCurrentLayerIndex(0, True)
-        sc.doc.Layers.Delete(sc.doc.Layers.FindName(layer_name), True)
-        
 def get_warning_message():
     """Returns warning messages or None if no warnings found
     """
@@ -290,10 +182,15 @@ def get_error_message():
     if len(layer_name) > 1:
         message = "Multiple layer names not supported"
     return message
-        
-deleteLayer(layer_name)
-        
+
+:q
+layer_name = layer_name.AllData()       
+if not layer_name: 
+    layer_name = "Entourage"
+else:
+    layer_name = layer_name[0]
+if not seed:
+    seed = RANDOM_SEED
+
 if place:
     populate(path, img_height, region, point, num)
-else:
-    deleteLayer(layer_name)
---
+++ src/auto_entourage/ghutil.py
@@ -0,0 +1,135 @@
+﻿"""Custom Grasshopper Utilities Module
+"""
+__author__ = "Vincent Mai"
+__version__ = "2020.10.05"
+
+import System
+import Rhino.RhinoDoc
+import scriptcontext as sc
+from Grasshopper import DataTree
+import ghpythonlib.treehelpers as th
+
+class RhinoDocContext:
+    """Context Manager to enter the RhinoDoc.ActiveDoc Context
+    """
+    def __init__(self):
+        self.ghdoc = sc.doc
+        self.rhinodoc = Rhino.RhinoDoc.ActiveDoc
+
+    def __enter__(self):
+        sc.doc = self.rhinodoc
+
+    def __exit__(self, type, value, traceback):
+        sc.doc = self.ghdoc
+
+class NewLayerContext:
+    """Context Manager to call a function in a new layer
+    """
+    def __init__(self, layer_name):
+        self.layer_name = layer_name
+
+    def __enter__(self):
+        self.__deleteLayer(self.layer_name)
+        self.__createAndSetCurrentLayer(self.layer_name)
+
+    def __exit__(self, type, value, traceback):
+        self.__resetLayer()
+
+    def __createAndSetCurrentLayer(self, layer_name):
+        """Creates a new layer and set it as the current layer
+        """
+        with RhinoDocContext():
+            layer = sc.doc.Layers.FindName(layer_name)
+            if layer is None:
+                layer_idx = sc.doc.Layers.Add(layer_name, System.Drawing.Color.Black)
+            else:
+                layer_idx = layer.Index
+            sc.doc.Layers.SetCurrentLayerIndex(layer_idx, True)
+            
+    def __resetLayer(self):
+        """Resets current layer to the default (first) layer
+        """
+        with RhinoDocContext():
+            sc.doc.Layers.SetCurrentLayerIndex(0, True)
+            
+    def __deleteLayer(self, layer_name):
+        """Deletes a layer and all objects inside
+        """
+        with RhinoDocContext():
+            rhinoObjects = sc.doc.Objects.FindByLayer(layer_name)
+            if rhinoObjects:
+                for obj in rhinoObjects:
+                    sc.doc.Objects.Delete(obj)
+            sc.doc.Layers.SetCurrentLayerIndex(0, True)
+            sc.doc.Layers.Delete(sc.doc.Layers.FindName(layer_name), True)
+
+class TreeHandler:
+    """Decorating class to handle trees as args for user define functions
+    """
+    def __init__(self, func):
+        self.func = func
+        
+    def __call__(self, *args, **kwargs):
+        args = map(TreeHandler.toTree, args)
+        for arg in args:
+            arg.SimplifyPaths()
+        depths = [self.__treeDepth(t) for t in args]
+        trees = [th.tree_to_list(arg, lambda x: x) for arg in args]
+        return th.list_to_tree(self.__ghtree_handler(trees, depths))
+
+    def __treeDepth(self, t):
+        """returns the depth of the gh tree"""
+        path = t.Path(t.BranchCount-1)
+        return len(str(path).split(";"))
+
+    def __intertwine(self, trees):
+        """intertwine two trees with equal depth
+        """
+        result = []
+        for i in range(max([len(t) for t in trees])): 
+            items = [t[min(i, len(t)-1)] for t in trees]
+            if isinstance(items[0], list):
+                result.append(self.__intertwine(items))
+            else:
+                result.append(self.func(*items))
+        return result
+
+    def __ghtree_handler(self, trees, depths):
+        """Returns appropriate GH Tree by explicit looping
+        """
+        max_depth = max(depths)
+        for idx, d in enumerate(depths):
+            for j in range(max_depth-d):
+                trees[idx] = [trees[idx]]
+        return self.__intertwine(trees)
+    
+    @staticmethod
+    def toTree(arg):
+        """Converts object to Grasshopper.DataTree
+        """
+        if isinstance(arg, DataTree[object]):
+            return arg
+        else:
+            if not isinstance(arg, list):
+                return th.list_to_tree([arg])
+            else:
+                return th.list_to_tree(arg)
+    
+    @staticmethod
+    def topologyTree(tree):
+        """Returns the tree's topology in an equivalently structure
+            Example:
+                >>> branchDataSize([[a, b, c], [x, y]])
+                >>> tree {1; 1}
+        """
+        def __listBranchSize(tree):
+            if isinstance(tree, DataTree[object]):
+                tree.SimplifyPaths()
+                tree = th.tree_to_list(tree, lambda x: x) 
+            result = []
+            for b in tree:
+                if isinstance(b[0], list):
+                    result.append(__listBranchSize(b))
+                result.append([len(b)])
+            return result
+        return th.list_to_tree(__listBranchSize(tree))
