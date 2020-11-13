e8a48e834ffbd40f173d465af5bcdc631e96c4bb
Vincent Mai <44751268+v-machine@users.noreply.github.com>
2020-11-13 12:42:03 -0500

    Update source code
    added `orient` functionality to instantaneously orient entourages to the current view angle

--- src/auto_entourage/auto_entourage.py
+++ src/auto_entourage/auto_entourage.py
@@ -1,18 +1,18 @@
 """Populates a given region (or ahcnor points) with entourages
     Inputs:
         path: File path to the image folder.
-        img_height: The entourage height in the model.
-        region: A region delineated by a closed curve.
-        num: The number of entourages within the region.
-        point: (Optional) The points where the entourage is anchored.
-        layer_name: Default to "Entourage" if not speficied.
+        imgHeight: The entourage height in the model.
+        point: The points where the entourage is anchored.
+        layerName: Default to "Entourage" if not speficied.
         seed: (Optional) Sets random seed. 
-        place: Loads entourages in a new layer.
+        load: Loads entourages in a new layer.
+        orient: (Re)orients the entourages to camera angle.
 """
 __author__ = "Vincent Mai"
-__version__ = "0.1.0"
+__version__ = "0.5.0"
 
 import System
+import math
 import Rhino
 import rhinoscriptsyntax as rs
 import scriptcontext as sc
@@ -26,26 +26,53 @@ from ghutil import RhinoDocContext, NewLayerContext, TreeHandler
 RANDOM_SEED = 0
 UNIT_Z = (0, 0, 1)
 
-def addPictureFrame(path, point, normal, width, height):
-    """Calls rhinoscriptsyntax's addPictureFrame method 
+class Struct:
+    """Cache the state of the loaded entourages
+    """
+    def __init__(self):
+        self.pictureframeIds = None
+        self.point = None
+        self.cameraDir = None
+
+    def cache(self, pictureframeIds=None, point=None, cameraDir=None):
+        """Caches the current state of the loaded entourages
+
+        Args:
+            pictureframeIds (gh.DataTree): guid of pictureframe
+            point (gh.DataTree): anchor points for pictureframes/entourages
+            cameraDir (rg.Vector3d): the cameraDirection of active viewport
+        """
+        if pictureframeIds:
+            self.pictureframeIds = pictureframeIds
+        if point:
+            self.point = point
+        if cameraDir:
+            self.cameraDir = cameraDir
+
+def addPictureFrame(path, point, orientation, width, height):
+    """Calls rhinoscriptsyntax's addPictureFrame method, return objectID
     
-    Creates a picture frame in Rhino and centers it around a given point. The 
-    picture frame will always be vertical and are not cached in grasshopper.
+    Creates a vertical picture frame in Rhino and centers it around the
+    anchor point.
     
     Args:
         path (str): path to .png image
-        point (rg.Point3d): anchor for picture frame
-        normal (rg.Vector3d): xy-orientation of the picture frame
+        point (rg.Point3d): anchor point for the picture frame
+        orietation (rg.Vector3d): the orientation of the picture frame
         width (float): width of the picture frame
         height (float): height of the picture fram
+    Returns:
+        (RhinoObjects.Id) the guid of the pictureframe
     """
     with RhinoDocContext():
-        x_axis = rs.VectorRotate(normal, 90, UNIT_Z)
-        y_axis = rg.Vector3d(*UNIT_Z)
-        point -= 0.5*x_axis*width
-        plane = rg.Plane(point, x_axis, y_axis)
-        sc.doc.ActiveDoc.Objects.AddPictureFrame(plane, path, False, width, 
-                                                 height, False, False)
+        xAxis = rs.VectorRotate(orientation, 90, UNIT_Z)
+        yAxis = rg.Vector3d(*UNIT_Z)
+        point -= 0.5*xAxis*width
+        plane = rg.Plane(point, xAxis, yAxis)
+        objId = sc.doc.ActiveDoc.Objects.AddPictureFrame(plane, path, False,
+                                                         width, height, False, False)
+        return objId 
+        
             
 def getFiles(path):
     """Returns a list of paths to PNGs from a directory
@@ -75,7 +102,7 @@ def scaleImage(baseWidth, baseHeight, targetHeight):
     Args:
         baseWidth (float): the original width of the .png file
         baseHeight (float): the original height of the .png file
-        targetHeight (float): the target height to scale to
+        targetHeight (float): the target height
     Returns:
         the scaled (width, height)
     """
@@ -83,7 +110,10 @@ def scaleImage(baseWidth, baseHeight, targetHeight):
     return baseWidth*factor, targetHeight
             
 def getCameraDirection():
-    """Returns the camera direction of the active viewport
+    """Returns the viewport camera direction (projected on XY plane)
+
+    Returns:
+        (rg.Vector3d) a vector representing the camera direction
     """
     cameraDir = sc.doc.Views.ActiveView.ActiveViewport.CameraDirection
     projCameraDir = rg.Vector3d(cameraDir.X, cameraDir.Y, 0)
@@ -91,37 +121,23 @@ def getCameraDirection():
     return projCameraDir
 
 @TreeHandler
-def populateRegion(region, num, seed):
-    """Returns a list of populated points given a region
-    
-    Args:
-        region (rg.Curve): a closed-curve delimiting a region
-        num (int): the number of points to populate the region
-        seed (int): random seed for random population
-    Returns:
-        a list of 3d points
-    """
-    brep = rg.Brep.CreatePlanarBreps(region)
-    return ghc.PopulateGeometry(brep, num, seed=seed)
-
-@TreeHandler
-def placeImage(path, pt, normal, img_height):
+def placeImage(path, point, orientation, imgHeight):
     """Orients, scales, and places the input image as a PictureFrame
     
-    Orients the input image based on normal, scales it to the target height 
-    and centers it on the input point.
+    Orients the input image based on orientation, scales it to the
+    target height and centers it on the anchor point.
    
     Args:
         img(str): path to the .png image 
-        pt (rg.Point3d): the anchor to center the PictureFrame
-        normal (rg.Vector3d): the normal vector of the PictureFrame
-        img_height (float): the target height to scale to
+        point (rg.Point3d): the anchor to center the PictureFrame
+        orientation (rg.Vector3d): the normal vector of the PictureFrame
+        imgHeight (float): the target height to scale to
     Notes:
         Skips images should they failed to be added to Rhino document
     """
     try:
-        width, height = scaleImage(*imageSize(path), targetHeight=img_height)
-        addPictureFrame(path, pt, normal, width, height)
+        width, height = scaleImage(*imageSize(path), targetHeight=imgHeight)
+        return addPictureFrame(path, point, orientation, width, height)
     except:
         print("Failed to process {}".format(path))
                     
@@ -131,9 +147,9 @@ def loadImage(path, num, seed):
     """Randomly choose a number of images from the given file path
     
     Args:
-        path (str): path to the directory contain the .png images
-        num (int): the number of images to load randomly
-        seed (int): random seed to randomize images
+        path (str): path to the image directory
+        num (int): the number of images to load
+        seed (int): random seed to randomize loading
     Returns:
         a list of images loaded from the given directory
     """
@@ -142,57 +158,97 @@ def loadImage(path, num, seed):
     imgList = random.sample(imgList, len(imgList))
     return [imgList[i % len(imgList)] for i in range(num)]
 
-def populate(path, img_height, region, point, num, seed):
-    """Populate a region (closed curve) with vertical PictureFrames
+def populate(path, imgHeight, point, layerName, seed, data):
+    """Populates a Rhino document with entourages (vertical PictureFrames)
+    and caches the current state
     
     Args:
         path (str): path to the directory of .png images 
-        img_height (float: the target height of the picture frame
-        region (rg.Curve): a closed curve
+        imgHeight (float: the target height of the picture frame
         point (rg.Point3d): the anchor point of the picture frame
-        num (int): the num of PictureFrames to populate a region
         seed (int): the random seed for loadImage and populateRegion
+        data (Struct): the current state of the entourages
     """
-    with NewLayerContext(layer_name):
+    with NewLayerContext(layerName):
         cameraDirection = getCameraDirection()
-        if region.AllData():
-            pts = populateRegion(region, num, seed)
-            imgs = loadImage(path, num, seed)
-            placeImage(imgs, pts, cameraDirection, img_height)
-        if point.AllData():
-            num = TreeHandler.topologyTree(point)
-            imgs = loadImage(path, num, seed)
-            placeImage(imgs, point, cameraDirection, img_height)
-       
-def get_warning_message():
+        num = TreeHandler.treeTopology(point)
+        imgs = loadImage(path, num, seed)
+        pfIds = placeImage(imgs, point, cameraDirection, imgHeight)
+        data.cache(pictureframeIds=pfIds, point=point,
+                   cameraDir=cameraDirection)
+
+def orientImages(data):
+    """(Re)orients existing entourages to a new camera angle and
+    caches new cameraDir
+
+    Args:
+        data: the cache data of entourages
+    """
+    @TreeHandler
+    def rotate(guid, center, angle):
+        rs.RotateObject(guid, center, angle)
+
+    with RhinoDocContext():
+        rs.EnableRedraw(False)
+        angle = orientAngle(data.cameraDir, getCameraDirection())
+        rotate(data.pictureframeIds, data.point, angle)
+        rs.EnableRedraw(True)
+        data.cache(cameraDir=getCameraDirection())
+    
+
+def orientAngle(v1, v2):
+    """Returns the signed angle for orienting v1 to v2
+
+    Args:
+        v1 (rg.Vector3d): the base vector
+        v2 (rg.Vector3d): the target vector
+    """
+    sign = lambda x: x and (1, -1)[x < 0]
+    angle = rg.Vector3d.VectorAngle(v1, v2)
+    cross = rg.Vector3d.CrossProduct(v1, v2)
+    direction = sign(rg.Vector3d.Multiply(cross, rg.Vector3d(*UNIT_Z)))
+    return direction * math.degrees(angle)
+
+def validInput():
+    if (not path.AllData() or
+        not imgHeight.AllData() or
+        not point.AllData() or
+        len(layerName.AllData()) > 1):
+        return False
+    return True
+    
+def getWarningMessage():
     """Returns warning messages or None if no warnings found
     """
     message = None
     if not path.AllData():
         message = "Path to PNGs is missing"
-    elif not img_height.AllData():
-        message = "img_height is missing"
-    elif not point.AllData() and not region.AllData():
-        message = "At least one region or one point is needed"
-    if region.AllData() and not num.AllData():
-        message = "Please specify num (of entourages) in region"
+    elif not imgHeight.AllData():
+        message = "imgHeight is missing"
+    elif not point.AllData():
+        message = "At least one point is needed"
     return message
 
-def get_error_message():
+def getErrorMessage():
     """Returns error messages or None if no errors found
     """
     message = None
-    if len(layer_name) > 1:
+    if len(layerName.AllData()) > 1:
         message = "Multiple layer names not supported"
     return message
 
-if not layer_name.AllData(): 
-    layer_name = "Entourage"
-else:
-    layer_name = layer_name.AllData()[0]
+if layerName.BranchCount == 0: 
+    layerName.Add("Entourage")
 
 if seed.BranchCount == 0:
-    seed = RANDOM_SEED
+    seed.Add(RANDOM_SEED)
 
-if place:
-    populate(path, img_height, region, point, num, seed)
+if load and validInput():
+    data = Struct()
+    populate(path, imgHeight, point, layerName.AllData()[0], seed, data)
+
+if orient:
+    try:
+        orientImages(data)
+    except NameError:
+        print("Entourages has not been loaded.")
--- src/auto_entourage/img_crop_component_src.py
+++ src/auto_entourage/img_crop_component_src.py
@@ -1,7 +1,7 @@
 ﻿"""Crops white spaces from PNG images.
     Inputs:
-        in_path: The path of the input directory
-        out_path: The path of the output directory
+        path: The path of the input directory
+        out: The path of the output directory
     Output:
         status: Returns the status of execution.
 """
@@ -12,54 +12,65 @@ from ghpythonlib.componentbase import executingcomponent as component
 import Grasshopper, GhPython
 import Grasshopper.Kernel as ghk
 import System
+import os
 import Rhino
 import subprocess
 
-class QuickCrop(component):
+class ImgCrop(component):
     
-    def RunScript(self, in_path, out_path, crop):
-        
-        status = None
-        
+    def __init__(self):
+        component.__init__(self)
+        self.status = ""
+        self.prevInput = None
+    
+    def RunScript(self, path, out, crop):
         # replace guid with compiled guid of the auto_entourage component
-        guid = System.Guid("9e1aaf9a-3496-42b2-a827-c9ae515b3226")
+        guid = System.Guid("8da439f7-c91d-4951-bef8-9a521b1d5add")
         auto_entourage = Grasshopper.Instances.ComponentServer.FindAssemblyByObject(guid)
-        quick_crop = auto_entourage.Location.replace("auto_entourage.ghpy",
-                                                     "quick_crop.exe")
+        img_crop = auto_entourage.Location.replace("auto_entourage.ghpy",
+                                                     "img_crop.exe")
         
-        def call_quick_crop():
-            """Calls quick_crop.exe and returns execution status.
+        def runImgCrop():
+            """Calls img_crop.exe and returns execution status.
             """
             startupinfo = subprocess.STARTUPINFO()
             startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
             startupinfo.wShowWindow = subprocess.SW_HIDE
             
-            pipe = subprocess.Popen([quick_crop, in_path, out_path], 
+            pipe = subprocess.Popen([img_crop, path, out], 
                                     startupinfo=startupinfo)
             pipe.communicate()
             
-            if pipe.returncode == 0:
-                return "Cropping is successful. Saved to {}".format(out_path)
-            else:
-                return "Cropping is unsuccessful. Nothing is saved."
+            return pipe.returncode
         
-        def get_warning_message():
+        def getWarningMessage():
             """Returns warning messages or None if no warnings found
             """
             message = None
-            if not in_path or not out_path:
-                message = "Both in_path and out_path needs to be specified"
+            if not path or not out:
+                message = "Both path and out needs to be specified"
             return message
             
-        def display_warnings():
-            warning_message = get_warning_message()
+        def displayWarnings():
+            warning_message = getWarningMessage()
             if warning_message is not None:
                 w = ghk.GH_RuntimeMessageLevel.Warning
                 self.AddRuntimeMessage(w, warning_message)
         
-        display_warnings()
+        displayWarnings()
         
-        if in_path and out_path and crop:
-            status = call_quick_crop()
+        if path and out:
+            curInput = hash((path, out))
+            if curInput != self.prevInput:
+                self.status = "ImaCrop hasn't been run."
+                self.prevInput = curInput
         
+        if crop:
+            if runImgCrop() == 0:
+                self.status = "Cropped {} (out of {}) images and saved to {}".format(
+                    len(os.listdir(out)), len(os.listdir(path)), out)
+            else:
+                self.status = "Unsuccessful. Nothing is saved."
+            
+        status = self.status
         return status
